69051d82a0bc1ac121a5ccfe645e07f9cfdc38da
Vinicius Assis <49870438+Viniciusassissilva@users.noreply.github.com>
2025-03-31 05:36:35 -0300

    configura whitenoise e coleta arquivos estáticos

--- cofre/settings.py
+++ cofre/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -161,8 +162,6 @@ BUFFER = 1024
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -173,4 +172,6 @@ CORS_ALLOW_ALL_ORIGINS = True  # Em produçao: cuidado!
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
